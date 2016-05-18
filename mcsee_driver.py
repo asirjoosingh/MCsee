8af15b784ebfd9e799466c4dc21770a321ea4039
Andrew Sirjoosingh (NU) <asirjoosingh@northwestern.edu>
2016-05-18 17:19:06 -0500

    Changed energy sorting/writing scheme in driver to save memory

--- mcsee_driver.py
+++ mcsee_driver.py
@@ -10,6 +10,7 @@ currdir=os.getcwd()
 Efermi,workfn,en0,coords0,theta0,phi0,nprimary,nelecperrun,fdiffinelcs,felf,decs,fimfp=readparams("params.in")
 stopen=Efermi+workfn # elecs with less than this energy cannot be transmitted
 
+# Get elastic cross section data
 os.chdir(decs)
 contents=os.listdir(".")
 ecsdata=[]
@@ -17,14 +18,17 @@ for item in contents:
   cs=ElScattCrossSec(item)
   ecsdata.append(cs)
 
+# Get inelastic cross section data
 os.chdir(currdir)
 ics=InelScattCrossSec(fimfp)
 ics.readicsdata(fdiffinelcs,felf,Efermi)
 
+# Initialize electron array
 elecs0=[]
 for i in range(nelecperrun):
   elecs0.append(Electron(0,en0,coords0,theta0,phi0,ecsdata,ics))
 
+# Iterate over number of cycles based on user-input total number of primary elecs vs elecs per cycle
 ncycles=nprimary/nelecperrun
 
 avgstr=""
@@ -33,7 +37,6 @@ nelectot=0
 nstoppedtot=0
 ntransmittedtot=0
 nsectransmittedtot=0
-emittedenergies=[]
 for i in range(ncycles):
   elecs=deepcopy(elecs0)
   nstopped,ntransmitted,nsectransmitted,emitteden=runmccycle(elecs,ecsdata,ics,Efermi,workfn,stopen)
@@ -43,7 +46,6 @@ for i in range(ncycles):
   nsectransmittedtot+=nsectransmitted
   for en in emitteden:
     enstr+="%10.6f\n"%(en)
-    emittedenergies.append(en)
   avgstr+="%15d%15d%15d%15d\n"%(nelecperrun,nstopped,ntransmitted,nsectransmitted)
   if i%10 == 0:
    avgout=open("running_avg.out","a")
@@ -62,7 +64,15 @@ enout=open("running_en.out","a")
 enout.write(enstr)
 enout.close()
 
+# Re-read output energy file and sort after finished to save memory
+os.system("sleep 5")
+enin=open("running_en.out","r")
+lines=enin.readlines()
+enin.close()
+emittedenergies=[float(line.strip()) for line in lines]
 emittedenergies.sort()
+
+# Output summary
 outp=open("emitted_energies.out","w")
 outp.write(str(nelectot) + " # Number of primary electrons\n")
 outp.write(str(nstoppedtot) + " # Number of stopped electrons\n")
